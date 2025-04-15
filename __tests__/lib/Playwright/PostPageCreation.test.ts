d31244858ae8ac6fdf87821cde28728f69dae0ec
alistaircc <160368040+alistaircc@users.noreply.github.com>
2025-04-14 20:46:15 -0400

    Update PostPageCreation.test.ts
    Updated secrets

--- __tests__/lib/Playwright/PostPageCreation.test.ts
+++ __tests__/lib/Playwright/PostPageCreation.test.ts
@@ -3,10 +3,8 @@ import { promises } from "fs";
 
 const BLUESKY_WEB_URL = 'https://bsky.app';
 
-//const BSKY_EMAIL = process.env.CIS565_EMAIL;
-//const BSKY_PASS = process.env.CIS565_PASSWORD;
-const BSKY_EMAIL = "cis565bskytests@gmail.com";
-const BSKY_PASS = "CIS565TestGroup"; 
+const BSKY_EMAIL = process.env.BS_USERNAME;
+const BSKY_PASS = process.env.BS_PASSWORD;
 
 // Global total post variable
 let TOTAL_POSTS = 0;
