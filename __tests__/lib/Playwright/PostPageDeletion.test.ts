d35e15363ab0efc34b7357ae6ec97075f91dfe10
alistaircc <160368040+alistaircc@users.noreply.github.com>
2025-04-15 19:29:48 -0400

    PR Post & Search Playwright Tests (#2)
    * Commit Post & Search Playwright Tests
    
    Pushing post and search playwright tests for the BlueSky web application to 'search-and-post-tests' branch
    
    * Update PostPageCancellation.test.ts
    
    Updated secrets
    
    * Update PostPageCreation.test.ts
    
    Updated secrets
    
    * Update PostPageCreation.test.ts
    
    Updated secrets
    
    * Update PostPageCreationEdgeCase.test.ts
    
    Updated secrets
    
    * Update PostPageDeletion.test.ts
    
    Updated secrets
    
    * Update SearchPageErrorCase.test.ts
    
    Updated secrets
    
    * Update SearchPageFeeds.test.ts
    
    Updated secrets
    
    * Update SearchPagePosts.test.ts
    
    Updated secrets
    
    * Update SearchPageUsers.test.ts
    
    Updated secrets
    
    * Create How To Deploy ReadMe.md
    
    * Update How To Deploy ReadMe.md
    
    * Update How To Deploy ReadMe.md
    
    * Update How To Deploy ReadMe.md
    
    * Create test-playwright.yml
    
    Add env variables to reference secrets for running playwright tests
    
    * Update PostPageCreation.test.ts
    
    Change secrets to be hardcoded for debugging purposes
    
    * Update test-playwright.yml
    
    * Update test-playwright.yml
    
    * Update PostPageCancellation.test.ts
    
    * Update PostPageCreation.test.ts
    
    * Update PostPageCreationEdgeCase.test.ts
    
    * Update PostPageDeletion.test.ts
    
    * Update SearchPageErrorCase.test.ts
    
    * Update SearchPageFeeds.test.ts
    
    * Update SearchPagePosts.test.ts
    
    * Update SearchPageUsers.test.ts
    
    * Update test-playwright.yml
    
    Run new jobs using updated secrets
    
    * Update PostPageDeletion.test.ts
    
    Updated to resolve failured in playwright tests
    
    * Update test-playwright.yml
    
    Update pipeline name to avoid matching the name of the one within the 'main' branch
    
    * Update How To Deploy ReadMe.md
    
    Updated readMe
    
    * Delete .github/workflows/test-playwright.yml
    
    Removing test-playwright file before merge to main branch

---
+++ __tests__/lib/Playwright/PostPageDeletion.test.ts
@@ -0,0 +1,137 @@
+import { test, expect, type Page } from '@playwright/test';
+
+const BLUESKY_WEB_URL = 'https://bsky.app';
+
+const BSKY_EMAIL = process.env.SECRET_USR;
+const BSKY_PASS = process.env.SECRET_PWD;
+
+
+// Login function
+async function login(page: Page): Promise<void> {
+
+  // Navigate to the login page
+  await page.goto(`${BLUESKY_WEB_URL}`);
+
+  // Click the sign-in button
+  await page.getByRole('button', { name: 'Sign in' }).click();
+
+  // Click on username field
+  await page.getByTestId('loginUsernameInput').click();
+
+  // Enter username
+  await page.getByTestId('loginUsernameInput').fill(`${BSKY_EMAIL}`);
+
+  // Click on password field
+  await page.getByTestId('loginPasswordInput').click();
+
+  // Enter pswd
+  await page.getByTestId('loginPasswordInput').fill(`${BSKY_PASS}`);
+
+  // Click on next button to sign-in
+  await page.getByTestId('loginNextButton').click();
+
+}
+
+// getDate function
+async function getDate() {
+
+  // Get current date
+  const currentDate = new Date();
+
+  // Extract month, day and year from currentDate obj
+  const monthIndex = currentDate.getMonth();
+  const day = currentDate.getDate();
+  const year = currentDate.getFullYear();
+
+  // Months of the year array list
+  const monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+  // Assign month name based on current month index
+  const monthOfYr = monthNames[monthIndex];
+
+  // Format the date in mm dd, yyyy format
+  const formatDate = `${monthOfYr} ${day}, ${year}`
+
+  // Return currentDate
+  return formatDate;
+
+}
+
+// Setup functionality
+test.beforeEach(async ({ page }) => {
+
+    // Call login function
+    await login(page);
+
+});
+
+// Delete last created Post
+test('Delete Last Created Post', {tag: '@post'}, async ({ page }) => {
+
+    // Get current date
+    const currentDate = new Date();
+
+    // Extract month, day and year from currentDate obj
+    const monthIndex = currentDate.getMonth();
+    const day = currentDate.getDate();
+    const year = currentDate.getFullYear();
+
+    // Months of the year array list
+    const monthNames = ['January','February','March','April','May','June','July','August','September','October','November','December'];
+
+    // Assign month name based on current month index
+    const monthOfYr = monthNames[monthIndex];
+
+    // Format the date in mm dd, yyyy format
+    const formatDate = `${monthOfYr} ${day}, ${year}`;
+
+    // Navigate to Profile
+    await page.getByRole('link', { name: 'Profile', exact: true }).click();
+
+    // Wait for 5 secs for page elems to full load
+    await page.waitForTimeout(5000);
+
+    // Get the number of posts
+    const post = await page.locator('xpath=//*[@id="root"]/div/div/div/div/div/main/div/div/div/div/div/div[2]/div/div[1]/div[1]/div/div[2]/div[3]/div/div').innerHTML();
+
+    // Split off the first element of variable
+    let postNum: number = parseInt(post.split("")[0]);
+
+    // Select Post tab
+    await page.getByTestId('profilePager-Posts').click();
+
+    // Wait for 5 secs for page elems to full load
+    await page.waitForTimeout(5000);
+
+    // Select ellipses for post based on current date
+    await page.getByRole('link', { name: `cis565testgroup.bsky.social\'s avatar View profileView profile · ${formatDate}` }).first().getByTestId('postDropdownBtn').click();
+
+    // Click delete this post
+    await page.getByTestId('postDropdownDeleteBtn').click();
+
+    // Confirm post deletion
+    await page.getByRole('button', { name: 'Delete' }).click();
+
+    // Wait for 5 secs for page elems to full load
+    await page.waitForTimeout(5000);
+
+    // Initiate a page reload to show upated post numbers
+    await page.reload();
+
+    // Navigate to profile (click on profile nav link)
+    await page.getByRole('link', { name: 'Profile', exact: true }).click();
+
+    // Get the number of posts
+    const postChk2 = await page.locator('xpath=//*[@id="root"]/div/div/div/div/div/main/div/div/div/div/div/div[2]/div/div[1]/div[1]/div/div[2]/div[3]/div/div').innerHTML();
+
+    // Split off the first element of variable
+    let actualNum: number = parseInt(postChk2.split("")[0]);
+ 
+    // Assertions
+    let expectedPosts = postNum -=1;
+
+    // Expected posts number to have decremented as latest post was deleted
+    await expect(actualNum).toEqual(expectedPosts);   
+
+
+});
---
+++ __tests__/lib/Playwright/SearchPagePosts.test.ts
@@ -0,0 +1,109 @@
+import { test, expect, type Page } from '@playwright/test';
+
+const BLUESKY_WEB_URL = 'https://bsky.app';
+
+const BSKY_EMAIL = process.env.SECRET_USR;
+const BSKY_PASS = process.env.SECRET_PWD;
+
+
+// Login function
+async function login(page: Page): Promise<void> {
+
+  // Navigate to the login page
+  await page.goto(`${BLUESKY_WEB_URL}`);
+
+  // Click the sign-in button
+  await page.getByRole('button', { name: 'Sign in' }).click();
+
+  // Click on username field
+  await page.getByTestId('loginUsernameInput').click();
+
+  // Enter username
+  await page.getByTestId('loginUsernameInput').fill(`${BSKY_EMAIL}`);
+
+  // Click on password field
+  await page.getByTestId('loginPasswordInput').click();
+
+  // Enter pswd
+  await page.getByTestId('loginPasswordInput').fill(`${BSKY_PASS}`);
+
+  // Click on next button to sign-in
+  await page.getByTestId('loginNextButton').click();
+
+}
+
+// Check if post exists, return boolean 
+async function chkPostExist(postText: string): Promise<boolean> {
+
+    // If postText is not a string, return false
+    if (typeof postText !== 'string' || !postText) {
+        return false;
+    }
+
+    // Else return
+    return postText.trim().includes(' ');
+}
+
+// Setup functionality
+test.beforeEach(async ({ page }) => {
+
+    // Call login function
+    await login(page);
+
+});
+
+
+// Search for posts using general search
+test('Search for posts using general search', {tag: '@search'}, async ({ page }) => {
+
+    // Search term
+    const searchTerm = 'pickleball';
+
+    // Navigate to search bar
+    await page.getByRole('search', { name: 'Search' }).click();
+
+    // Type searchTerm
+    await page.getByRole('search', { name: 'Search' }).fill(searchTerm);
+
+    // Stimulate pressing Enter key on keyboard
+    await page.getByRole('search', { name: 'Search' }).press('Enter');
+
+    // Select the first post description seen
+    let postText = await page.getByTestId('postText').first().textContent();
+
+    // Check that post has no than one word
+    let actual = await chkPostExist(postText);
+
+    // Assertion
+    // Verify post description has no than one word
+    await expect(actual).toEqual(true);
+
+});
+
+
+// Search for post within user profile
+test('Search for post within user profile', {tag: '@search'}, async ({ page }) => {
+
+    // Search term
+    const profile = '@majorleaguepb.bsky.social'
+
+    // Navigate to search bar
+    await page.getByRole('search', { name: 'Search' }).click();
+
+    // Search using profile handle
+    await page.getByRole('search', { name: 'Search' }).fill(profile);
+    
+    // Click on exact profile handle
+    await page.getByRole('link', { name: 'majorleaguepb.bsky.social', exact: true }).click();
+
+    // Select the first post description seen
+    let postText = await page.getByTestId('postText').first().textContent();
+
+    // Check that post has more than one word
+    let actual = await chkPostExist(postText);
+
+    // Assertion
+    // Verify post description has more than one word
+    await expect(actual).toEqual(true);
+
+});
